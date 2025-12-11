cad4dfc51a3167970dbd7d2671572bf155c073cb
Nuray <nuraiismagul2008@gmail.com>
2025-12-11 08:45:20 +0500

    Hospital Managment 1 week

---
+++ src/com/hospital/management/Main.java
@@ -0,0 +1,19 @@
+package com.hospital.management;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("---------------==◎◉◎==---------------");
+        System.out.println("Welcome to Hospital Management System");
+        System.out.println("---------------==◎◉◎==---------------");
+        System.out.println();
+        System.out.println("This system will help manage hospital operations");
+        System.out.println("Features(It will (probably) change):");
+        System.out.println("1. Patient Registration");
+        System.out.println("2. Doctor Scheduling");
+        System.out.println("3. Appointment Booking");
+        System.out.println("4. Medical Records");
+        System.out.println("5. Billing System");
+        System.out.println();
+        System.out.println("Course: Object-Oriented Programming - Week 1");
+    }
+}
